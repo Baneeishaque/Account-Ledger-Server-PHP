0666f0721db2e1fb44fdd6ef90b04c72ab28c196
dk <dk@dk-PC>
2018-05-27 18:35:12 +0530

    db

---
+++ http_API/insert_Transaction_patch.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once 'config.php';
+
+$event_date_time = filter_input(INPUT_POST, 'event_date_time');
+$user_id = filter_input(INPUT_POST, 'user_id');
+$particulars = filter_input(INPUT_POST, 'particulars');
+$amount = filter_input(INPUT_POST, 'amount');
+
+$sql="INSERT INTO `transactions`(`event_date_time`, `particulars`, `amount`, `insertion_date_time`, `inserter_id`) VALUES ('$event_date_time','$particulars','$amount',CONVERT_TZ(NOW(),'-05:30','+00:00'),'$user_id')";
+
+if (!$con->query($sql)) {
+    $arr = array('status' => "1", 'error' => $con->error);
+} else {
+    $arr = array('status' => "0");
+}
+echo json_encode($arr);
