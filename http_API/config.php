a271cacd76f61c1ed90c87a09143a53ab57f4230
Banee Ishaque K <baneeishque@gmail.com>
2018-11-20 22:59:15 +0530

    DB Clean

--- http_API/config.php
+++ http_API/config.php
@@ -1,8 +1,9 @@
 <?php
 
-$cpanel_username = "vfmobo6d";
+//$cpanel_username = "vfmobo6d";
 //echo $cpanel_username;
 //$con = new mysqli("localhost", "edcccet_ndk", "9895204814", "edcccet_lottery");
 //$con = new mysqli("localhost", "vfmobo6d_ndk", "9895204814", "vfmobo6d_account_ledger");
-$con = new mysqli("localhost", $cpanel_username . "_ndk", "aA9895204814", $cpanel_username . "_account_ledger");
+//$con = new mysqli("localhost", $cpanel_username . "_ndk", "aA9895204814", $cpanel_username . "_account_ledger");
 //echo $cpanel_username . "_ndk";
+$con = new mysqli("localhost", "root", "aA!9895204814", "account_ledger");
