2290a4fd76e20d0a28dd1bb71a8391876f94b0c2
Banee Ishaque K <baneeishaque@gmail.com>
2019-01-08 14:34:53 +0530

    want to sort by event date time

--- http_API/select_User_Transactions_v3.php
+++ http_API/select_User_Transactions_v3.php
@@ -11,42 +11,34 @@ include_once 'config.php';
 $user_id = filter_input(INPUT_GET, 'user_id');
 $account_id = filter_input(INPUT_GET, 'account_id');
 
-$get_accounts_sql = "SELECT `account_id` FROM `accounts` WHERE `parent_account_id`='$account_id' ORDER BY `account_id`";
-
-$get_accounts_sql_result = $con->query($get_accounts_sql);
-
 $empty_array = array();
 
-if (mysqli_num_rows($get_accounts_sql_result) != 0) {
+function recursive_select_user_transactions($local_account_id)
+{
+    $get_accounts_sql = "SELECT `account_id` FROM `accounts` WHERE `parent_account_id`='$local_account_id' ORDER BY `account_id`";
 
-//    array_push($empty_array, array("status" => "0"));
+    $get_accounts_sql_result = $GLOBALS['con']->query($get_accounts_sql);
 
-    while ($get_accounts_sql_result_row = mysqli_fetch_assoc($get_accounts_sql_result)) {
-//        $empty_array[] = $status_row;
-        //TODO : Iterate Child A/cs
+    if (mysqli_num_rows($get_accounts_sql_result) != 0) {
+
+        while ($get_accounts_sql_result_row = mysqli_fetch_assoc($get_accounts_sql_result)) {
+
+            recursive_select_user_transactions($get_accounts_sql_result_row['account_id']);
+        }
     }
-} else {
-//    array_push($empty_array, array("status" => "1"));
 
+    $get_transactions_sql = "SELECT `id`,`event_date_time`, `particulars`, `amount`, `insertion_date_time`,(SELECT name FROM accounts WHERE accounts.account_id=from_account_id) AS `from_account_name`,(SELECT full_name FROM accounts WHERE accounts.account_id=from_account_id) AS `from_account_full_name`,`from_account_id`,(SELECT name FROM accounts WHERE accounts.account_id=to_account_id) AS `to_account_name`,(SELECT full_name FROM accounts WHERE accounts.account_id=to_account_id) AS `to_account_full_name`,`to_account_id` FROM `transactionsv2` WHERE `inserter_id`='" . $GLOBALS['user_id'] . "' AND (`from_account_id`='$local_account_id' OR `to_account_id`='$local_account_id') ORDER BY `transactionsv2`.`event_date_time`";
 
-    $get_transactions_sql = "SELECT `id`,`event_date_time`, `particulars`, `amount`, `insertion_date_time`,(SELECT name FROM accounts WHERE accounts.account_id=from_account_id) AS `from_account_name`,(SELECT full_name FROM accounts WHERE accounts.account_id=from_account_id) AS `from_account_full_name`,`from_account_id`,(SELECT name FROM accounts WHERE accounts.account_id=to_account_id) AS `to_account_name`,(SELECT full_name FROM accounts WHERE accounts.account_id=to_account_id) AS `to_account_full_name`,`to_account_id` FROM `transactionsv2` WHERE `inserter_id`='$user_id' AND (`from_account_id`='$account_id' OR `to_account_id`='$account_id') ORDER BY `transactionsv2`.`event_date_time`";
-
-    $get_transactions_sql_result = $con->query($get_transactions_sql);
-
-//    $emptyarray = array();
+    $get_transactions_sql_result = $GLOBALS['con']->query($get_transactions_sql);
 
     if (mysqli_num_rows($get_transactions_sql_result) != 0) {
 
-//        array_push($emptyarray, array("status" => "0"));
-
         while ($get_transactions_sql_result_row = mysqli_fetch_assoc($get_transactions_sql_result)) {
-            $empty_array[] = $get_transactions_sql_result_row;
+            $GLOBALS['empty_array'][] = $get_transactions_sql_result_row;
         }
     }
-//    else {
-//        array_push($emptyarray, array("status" => "2"));
-//    }
 
 }
 
+recursive_select_user_transactions($account_id);
 echo json_encode($empty_array);
