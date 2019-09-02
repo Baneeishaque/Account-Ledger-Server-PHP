90e0148940e5ab599853c3afbc602856e892b383
Banee Ishaque K <baneeishaque@gmail.com>
2019-09-02 15:30:38 +0530

    Local IIS

--- http_API/select_User.php
+++ http_API/select_User.php
@@ -2,6 +2,6 @@
 
 include_once 'config.php';
 
-//echo filter_input(INPUT_GET,'username').','.filter_input(INPUT_GET,'password');
+// echo filter_input(INPUT_GET,'username').','.filter_input(INPUT_GET,'password');
 
 echo json_encode(mysqli_fetch_assoc($con->query("SELECT COUNT(`username`) AS `user_count`, `id` FROM `users` WHERE `username`='" . filter_input(INPUT_GET, 'username') . "' AND `password`='" . filter_input(INPUT_GET, 'password') . "'")));
