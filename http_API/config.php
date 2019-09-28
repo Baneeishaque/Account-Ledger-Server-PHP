749f127eec5f10bb12597279ef0147a25390be5f
Banee Ishaque K <Baneeishaque@gmail.com>
2019-09-28 11:55:34 +0530

    Adgarage & dbs

--- http_API/config.php
+++ http_API/config.php
@@ -9,4 +9,4 @@ ini_set('display_errors', '1');
 //$con = new mysqli("localhost", "root", "aA!9895204814", "account_ledger");
 //$con = new mysqli("fdb20.awardspace.net", "3140855_account", "aA!9895204814", "3140855_account");
 // $con = new mysqli("sql205.byethost.com", "b14_24405831", "w5qUz@F7WgaPNqF", "b14_24405831_account_ledger");
-$con = new mysqli("localhost", "root", "1234", "account_ledger");
+$con = new mysqli("localhost", "adgarage_root", "aA9895204814", "adgarage_wordpress");
