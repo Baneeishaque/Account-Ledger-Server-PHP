4e63f359edb547422894a9b0e346128526a7dce4
Banee ishaque K <baneeishaque@gmail.com>
2020-09-04 16:32:32 +0530

    Debug

--- http_API/select_User_Transactions_v3.php
+++ http_API/select_User_Transactions_v3.php
@@ -41,13 +41,14 @@ function recursive_select_user_transactions($local_account_id)
 }
 
 recursive_select_user_transactions($account_id);
-if(empty($empty_array))
-{
-	array_unshift($empty_array,"status"=>"2");
-	echo json_encode($empty_array);
-}
-else
-{
-	array_unshift($empty_array,"status"=>"0");
-	echo json_encode($empty_array);
-}
+var_dump($empty_array);
+// if(empty($empty_array))
+// {
+	// array_unshift($empty_array,"status"=>"2");
+	// echo json_encode($empty_array);
+// }
+// else
+// {
+	// array_unshift($empty_array,"status"=>"0");
+	// echo json_encode($empty_array);
+// }
