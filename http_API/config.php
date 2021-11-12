b8c850be223307e2b60f0362f258ac34096196b0
Banee Ishaque K <baneeishaque@gmail.com>
2021-11-12 19:25:58 +0530

    Format Code

--- http_API/config.php
+++ http_API/config.php
@@ -1,9 +1,7 @@
 <?php
 
 ini_set('display_errors', '1');
-
 error_reporting(E_ALL);
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
 $con = new mysqli($url["host"], $url["user"], $url["pass"], substr($url["path"], 1));
